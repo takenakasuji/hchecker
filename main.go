98a1f49662edbd9065b01295005d96e8ff3c09e5
takenakasuji <t.nksj.e.89@gmail.com>
2018-04-21 21:56:12 +0900

    fix: go fmt

--- main.go
+++ main.go
@@ -2,43 +2,43 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
-    "net/http"
-    "flag"
+	"net/http"
 )
 
 // Response struct
 type Response struct {
-    Status string `json:"status"`
-    StatusCode int `json:"status_code"`
-    Header map[string][]string `json:"header"`
+	Status     string              `json:"status"`
+	StatusCode int                 `json:"status_code"`
+	Header     map[string][]string `json:"header"`
 }
 
 func main() {
 
-    var (
-        strURL = flag.String("url", "", "help message for \"url\" option")
-    )
+	var (
+		strURL = flag.String("url", "", "help message for \"url\" option")
+	)
 
-    flag.Parse()
+	flag.Parse()
 
-    if *strURL == "" {
-        fmt.Println("need set url")
-        return
-    }
+	if *strURL == "" {
+		fmt.Println("need set url")
+		return
+	}
 
 	resp, err := http.Get(*strURL)
 	if err != nil {
 		log.Fatal(err)
-    }
+	}
 
-    res := Response {
-        Status: resp.Status,
-        StatusCode: resp.StatusCode,
-        Header: resp.Header,
-    }
+	res := Response{
+		Status:     resp.Status,
+		StatusCode: resp.StatusCode,
+		Header:     resp.Header,
+	}
 
-    JSON, _ := json.Marshal(res)
-    fmt.Println(string(JSON))
+	JSON, _ := json.Marshal(res)
+	fmt.Println(string(JSON))
 }
